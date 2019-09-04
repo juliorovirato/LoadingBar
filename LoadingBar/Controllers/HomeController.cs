105f3d3735d278a39d674b9a29adefc2112b41c9
Julio Rovira <julio.rovirato@amigo.edu.co>
2019-09-04 15:56:31 -0500

    version 1.1

---
+++ LoadingBar/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LoadingBar.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LoadingBar.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult Validar()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
